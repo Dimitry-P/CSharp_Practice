ba35612ab7cec995d0ec69baa42360c439f1f45b
Dimitry <dimitry.piatnitsky@yahoo.com>
2025-11-07 09:31:17 +0300

    Task02_Password_Validator

---
+++ Task02_Password_Validator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    public class Class1
+    {
+        public static bool Password(string st)
+        {
+            bool isUpper = false;
+            bool isLower = false;
+            bool isDigit = false;
+
+            if (st.Length >= 8)
+            {
+                for (int i = 0; i < st.Length; i++)
+                {
+                    if (char.IsUpper(st[i]))
+                        isUpper = true;
+
+                    if (char.IsLower(st[i]))
+                        isLower = true;
+
+                    if (char.IsDigit(st[i]))
+                        isDigit = true;
+                }
+                return isLower && isUpper && isDigit;
+            }
+            return false;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string userInput = Console.ReadLine();
+
+            bool result = Password(userInput);
+
+            if (result)
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                Console.WriteLine("Password is NOT valid");
+            }
+        }
+    }
+}
