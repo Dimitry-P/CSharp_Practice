da5091fb49b57a79a23b59b30dfc8a4ebebba286
Dimitry <dimitry.piatnitsky@yahoo.com>
2025-11-09 17:26:39 +0300

    Task08_Added_solution_for_Person_Validation: Program.cs + Person_Class.cs

---
+++ Task08_Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        // 1. Классы и свойства
+        // Задача:
+        //Создайте класс Person, представляющий человека.У него должны быть свойства:
+        //Name (имя, string),
+        //Age (возраст, int, должен быть больше 0).
+        //Добавьте метод для вывода информации о человеке.
+
+        //Пример:
+        //Ввод: Name = "Alice", Age = 25
+        //Вывод: Name: Alice, Age: 25
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя и возраст:");
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Person person1 = new Person(name, age);
+            person1.ShowNameAge();
+        }
+    }
+}
