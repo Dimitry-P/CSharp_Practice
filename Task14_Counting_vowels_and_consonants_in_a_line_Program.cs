046a976ae614ac56147e56c9b5710fab12e8497c
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-13 21:50:11 +0300

    Task14: Counting vowels and consonants in a line (Program.cs + StringAnalyzer.cs)

---
+++ Task14_Counting_vowels_and_consonants_in_a_line_Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp_Practice
+{
+    class Program
+    {
+        public static string input;
+        public static void Main()
+        {
+            Console.WriteLine("Введите какое-то выражение:");
+            input = Console.ReadLine();
+            StringAnalyzer.CountLettersAmount(input);
+        }
+    }
+}
+
+//Подсчёт гласных и согласных в строке
+//Описание:
+//Создайте класс StringAnalyzer, который анализирует строку. Реализуйте:
+
+//Подсчёт количества гласных букв.
+//Подсчёт количества согласных букв.
+//Исключение, если строка пуста или null.
+//Классы:
+
+//StringAnalyzer – содержит методы:
+//CountVowels(string input) – возвращает количество гласных.
+//CountConsonants(string input) – возвращает количество согласных.
+//Пример ввода:
+//string input = "Hello, World!";
+
+//Вывод:
+//Количество гласных: 3
+//Количество согласных: 7
---
+++ Task14_Counting_vowels_and_consonants_in_a_line_StringAnalyzer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace CSharp_Practice
+{
+    public class StringAnalyzer
+    {
+        public static int vowels = 0;
+        public static int consonants = 0;
+        public static int x = 0;
+
+        public static void CountLettersAmount(string input)
+        {
+            if (input.Length != 0)
+            {
+                CountVowels(input);
+                CountConsonants(input);
+                Console.WriteLine("Количество гласных: " + vowels);
+                Console.WriteLine("Количество согласных: " + consonants);
+                x = 1;
+            }
+            try
+            {
+                int y = 1 / x;
+            }
+            catch
+            {
+                Console.WriteLine("строка пуста");
+                return;
+            }
+        }
+        public static void CountVowels(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string oneLetterOfInput = input[i].ToString();
+                string allWovels = "aeiouAEIOU";
+                for (int k = 0; k < allWovels.Length; k++)
+                {
+                    if (oneLetterOfInput == allWovels[k].ToString())
+                    {
+                        vowels++;
+                    }
+                }
+            }
+        }
+
+        public static void CountConsonants(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                string oneLetterOfInput = input[i].ToString();
+                string allConsonants = "BCDFGHJKLMNPQRSTVWXZ";
+                for (int k = 0; k < allConsonants.Length; k++)
+                {
+                    if (oneLetterOfInput == allConsonants[k].ToString().ToLower() || oneLetterOfInput == allConsonants[k].ToString())
+                    {
+                        consonants++;
+                    }
+                }
+            }
+        }
+        //CountVowels(string input) – возвращает количество гласных.
+        //CountConsonants(string input) – возвращает количество согласных.
+    }
+}
+
+//Пример ввода:
+//string input = "Hello, World!";
+
+//Вывод:
+//Количество гласных: 3
+//Количество согласных: 7
