c59cd4cbdfee19316b8ef36461120c5cf8778441
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-14 22:29:14 +0300

    Task17: Text Editor (Program.cs + TextEditor.cs)

---
+++ Task17_TextEditor_Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextEditor newEditor = new TextEditor();
+            newEditor.TypeText("Hello");
+            newEditor.TypeText("World");
+            Console.WriteLine(newEditor.GetText());
+            newEditor.Undo();
+            Console.WriteLine(newEditor.GetText());
+            newEditor.Undo();
+            Console.WriteLine(newEditor.GetText());
+        }
+    }
+}
---
+++ Task17_TextEditor_TextEditor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    class TextEditor
+    {
+        private Stack<string> stack = new Stack<string>();
+        private string newText = "";
+        public void TypeText(string text)
+        {
+            stack.Push(text);
+            newText += text;
+        }
+        public void Undo()
+        {
+            if (stack.Count > 0)
+            {
+                newText = stack.Pop();
+            }
+        }
+        public string GetText()
+        {
+            return newText;
+        }
+    }
+}
