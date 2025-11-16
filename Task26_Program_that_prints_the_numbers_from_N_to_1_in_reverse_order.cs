4cdfa564c40e6b6a994819aead7b2b9f83de9787
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-16 22:44:16 +0300

    Task26: Program that prints the numbers from N to 1 in reverse order

---
+++ Task26_Program_that_prints_the_numbers_from_N_to_1_in_reverse_order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 7: Напишите программу, которая выводит числа от N до 1 в обратном порядке.
+        public static string number;
+        public static int result;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 1");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = result; i > 0; i--)
+            {
+                if (i == 1)
+                {
+                    Console.Write(i);
+                    return;
+                }
+                Console.Write(i + ", ");
+            }
+        }
+    }
+}
