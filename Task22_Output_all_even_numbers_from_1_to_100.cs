9f38f8a4cc8b40e43b6e69db6f984442e64189ce
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-16 22:24:17 +0300

    Task22: Output all even numbers from 1 to 100

---
+++ Task22_Output_all_even_numbers_from_1_to_100.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        // •	Задача 3: Напишите программу, которая выводит все четные числа от 1 до 100.
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
