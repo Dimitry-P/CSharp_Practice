2840b791ba791744fe132007335aeb4681d8a858
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-16 22:40:37 +0300

    Task25: Program that finds the largest of N numbers entered by the user

---
+++ Task25_Program_that_finds_the_largest_of_N_numbers_entered_by_the_user.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 6: Напишите программу, которая
+        //находит наибольшее из N чисел, введенных пользователем.
+        public static string array;
+        public static int result = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите несколько чисел через запяту");
+            array = Console.ReadLine();
+            string[] pieces = array.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (result < int.Parse(pieces[i]))
+                {
+                    result = int.Parse(pieces[i]);
+                }
+            }
+            Console.WriteLine("Наибольшее число: " + result);
+        }
+    }
+}
