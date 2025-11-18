56c3da95a8f6b8018b9c55eb4a3dada16f769ef2
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-18 09:23:25 +0300

    Task27: Program that calculates the sum of numbers from 1 to N using a for loop

---
+++ Task27_Program_that_calculates_the_sum_of_numbers_from_1_to_N_using_a_for_loop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        public static string number;
+        public static int result;
+        public static int sum = 0;
+
+        //•	Задача 8: Напишите программу, которая вычисляет
+        //сумму чисел от 1 до N, используя цикл for.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 1");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = 1; i < result + 1; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Сумма чисел равна: " + sum);
+        }
+    }
+}
