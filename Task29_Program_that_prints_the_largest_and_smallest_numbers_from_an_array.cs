4b570f1fcc7524035961da840a7f6a0c933f9f3e
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-18 09:30:12 +0300

    Task29: Program that prints the largest and smallest numbers from an array

---
+++ Task29_Program_that_prints_the_largest_and_smallest_numbers_from_an_array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        public static int[] Array = new int[9] { 5, 1, 66, 8, 2, 13, 9, 22, 45 };
+        public static int min = 0;
+        public static int max = 0;
+
+        //•	Задача 10: Напишите программу, которая
+        //выводит наибольшие и наименьшие числа из массива.
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (max < Array[i])
+                {
+                    max = Array[i];
+                }
+            }
+            min = Array[0];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (min > Array[i])
+                {
+                    min = Array[i];
+                }
+            }
+            Console.WriteLine("Наименьшее число: " + min + " Наибольшее число: " + max);
+        }
+    }
+}
