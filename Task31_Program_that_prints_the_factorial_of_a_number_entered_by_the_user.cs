649a2ce49fabe4e90e6d4b5f40e008b0625a4036
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-18 09:38:02 +0300

    Task31: Program that prints the factorial of a number entered by the user

---
+++ Task31_Program_that_prints_the_factorial_of_a_number_entered_by_the_user.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 12: Напишите программу, которая выводит
+        //факториал числа, введенного пользователем.
+        public static string number;
+        public static int result;
+        public static int sum = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 0");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = result; i > 0; i--)
+            {
+                sum *= i;
+            }
+            Console.WriteLine("Факториал равен: " + sum);
+        }
+    }
+}
