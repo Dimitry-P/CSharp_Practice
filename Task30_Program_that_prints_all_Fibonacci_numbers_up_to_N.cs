b84d6bfcba9e6607dc950979bd0d4d5f21c0ca78
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-18 09:33:37 +0300

    Task30: Program that prints all Fibonacci numbers up to N

---
+++ Task30_Program_that_prints_all_Fibonacci_numbers_up_to_N.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 11: Напишите программу, которая выводит все числа Фибоначчи до N.
+        public static string number;
+        public static int result;
+        public static int first;
+        public static int second;
+        public static int third;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число больше 0");
+            number = Console.ReadLine();
+            result = int.Parse(number);
+            for (int i = 0; i < result; i++)
+            {
+                if (i == 0)
+                {
+                    first = 0;
+                    second = 1;
+                    Console.Write(first + ", ");
+                    Console.Write(second + ", ");
+                }
+                if (i != 0)
+                {
+                    third = first + second;
+                    Console.Write(third + ", ");
+                    if (third == result) return;
+                    first = second;
+                    second = third;
+                }
+            }
+        }
+    }
+}
