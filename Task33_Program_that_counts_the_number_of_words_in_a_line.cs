5762804d442ca7a8962bfe1348d7f2bcc167efb5
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-19 22:58:27 +0300

    Task33: Program that counts the number of words in a line

---
+++ Task33_Program_that_counts_the_number_of_words_in_a_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        public static string phrase;
+        //•	Задача 14: Напишите программу, которая считает количество слов в строке.
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            phrase = Console.ReadLine();
+            string[] pieces = phrase.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("количество слов в строке: " + pieces.Length);
+        }
+    }
+}
