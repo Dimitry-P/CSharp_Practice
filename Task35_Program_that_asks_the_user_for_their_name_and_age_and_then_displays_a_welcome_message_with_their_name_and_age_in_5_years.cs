245703a8c34b2fce852ec51e686f594e8060d622
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-21 09:30:53 +0300

    Task35: Program that asks the user for their name and age and then displays a welcome message with their name and age in 5 years

---
+++ Task35_Program_that_asks_the_user_for_their_name_and_age_and_then_displays_a_welcome_message_with_their_name_and_age_in_5_years.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 1: Напишите программу, которая запрашивает
+    //у пользователя его имя и возраст, а затем выводит
+    //приветственное сообщение с его именем и возрастом через 5 лет.
+    internal class Program
+    {
+        public static string age;
+        public static string name;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сообщите, пожалуйста, ваше имя и возраст:");
+            Console.Write("имя:");
+            name = Console.ReadLine();
+            Console.Write("возраст:");
+            age = Console.ReadLine();
+            int number = int.Parse(age);
+            number += 5;
+            Console.WriteLine("Приветствую, " + name + ", ваш возраст через пять лет: " + number);
+
+            Console.ReadKey();
+        }
+    }
+}
