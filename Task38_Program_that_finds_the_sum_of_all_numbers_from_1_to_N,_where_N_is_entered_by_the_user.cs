e880b2702ab7b02064d2a5a0a270e075ff123249
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-21 09:45:09 +0300

    Task38: Program that finds the sum of all numbers from 1 to N, where N is entered by the user

---
+++ Task38_Program_that_finds_the_sum_of_all_numbers_from_1_to_N,_where_N_is_entered_by_the_user.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 4: Напишите программу, которая находит
+    //сумму всех чисел от 1 до N, где N вводится пользователем.
+    internal class Program
+    {
+        public static string number;
+        public static int result = 0;
+        public static int LastNumber;
+
+        public static int sum(int number)
+        {
+            while (LastNumber != 0)
+            {
+                result += LastNumber;
+                LastNumber--;
+            }
+            return result;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Введите число больше единицы");
+            number = Console.ReadLine();
+            LastNumber = int.Parse(number);
+            while (LastNumber < 0)
+            {
+                Console.WriteLine("Вы ввели неверное число");
+                Console.WriteLine("Введите число больше единицы");
+                number = Console.ReadLine();
+                LastNumber = int.Parse(number);
+            }
+            sum(LastNumber);
+            Console.WriteLine("Сумма всех чисел от одного до вашего числа равна " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
