41ebd6ec23ad2b38e6e79745186b78e9061397e4
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-21 09:23:18 +0300

    Task34: Program that counts how many times the letter 'a' appears in a string

---
+++ Task34_Program_that_counts_how_many_times_the_letter_'a'_appears_in_a_string.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        public static string phrase;
+        public static string letter = "а";
+        public static int timer = 0;
+
+        //•	Задача 15: Напишите программу, которая подсчитывает,
+        //сколько раз буква 'a' встречается в строке.+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            phrase = Console.ReadLine();
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                if (phrase[i] == char.Parse(letter)) timer++;
+            }
+            Console.WriteLine("Буква а встречается в строке: " + timer + " раз");
+        }
+    }
+}
