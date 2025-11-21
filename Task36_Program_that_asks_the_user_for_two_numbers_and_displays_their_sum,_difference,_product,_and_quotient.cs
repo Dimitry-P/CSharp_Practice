f0f8a3a6ff93799c48382f8e541d40ad7205d05e
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-21 09:34:56 +0300

    Task36: Program that asks the user for two numbers and displays their sum, difference, product, and quotient

---
+++ Task36_Program_that_asks_the_user_for_two_numbers_and_displays_their_sum,_difference,_product,_and_quotient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        // •	Задача 2: Напишите программу, которая запрашивает
+        // у пользователя два числа и выводит их сумму, разницу,
+        // произведение и частное.
+        public static string number1;
+        public static string number2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            number1 = Console.ReadLine();
+            int firstNumber = int.Parse(number1);
+            Console.WriteLine("Введите второе число:");
+            number2 = Console.ReadLine();
+            int secondNumber = int.Parse(number2);
+            Console.WriteLine("Сумма двух чисел равна " + (firstNumber + secondNumber));
+            Console.WriteLine("Разница двух чисел равна " + (firstNumber - secondNumber));
+            Console.WriteLine("Произведение двух чисел равно " + (firstNumber * secondNumber));
+            Console.WriteLine("Частное двух чисел равно " + (firstNumber / secondNumber));
+
+            Console.ReadKey();
+        }
+    }
+}
