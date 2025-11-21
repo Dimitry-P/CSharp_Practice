71fdeeac75bba63812a06949aae92d382412d0c5
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-21 09:40:41 +0300

    Task37: Program that calculates the perimeter of a rectangle given its length and width

---
+++ Task37_Program_that_calculates_the_perimeter_of_a_rectangle_given_its_length_and_width.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 3: Напишите программу, которая вычисляет периметр
+    //прямоугольника, зная его длину и ширину.
+    internal class Program
+    {
+        public static string length;
+        public static string width;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину прямоугольника");
+            length = Console.ReadLine();
+            int lengthOfFigure = int.Parse(length);
+            Console.WriteLine("Введите ширину прямоугольника");
+            width = Console.ReadLine();
+            int widthOfFigure = int.Parse(width);
+            Console.WriteLine("Периметр прямоугольника равен " + (lengthOfFigure * 2 + widthOfFigure * 2));
+
+            Console.ReadKey();
+        }
+    }
+}
