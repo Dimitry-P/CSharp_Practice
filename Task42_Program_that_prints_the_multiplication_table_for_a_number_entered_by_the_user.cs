faae3494a9872578090c54ceb56a0b074f2e3329
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-22 09:33:53 +0300

    Task42: Program that prints the multiplication table for a number entered by the user

---
+++ Task42_Program_that_prints_the_multiplication_table_for_a_number_entered_by_the_user.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 8: Напишите программу, которая выводит таблицу
+        //умножения для числа, введенного пользователем.
+        public static string num;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            num = Console.ReadLine();
+            int number = int.Parse(num);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{number} * {i + 1} = {number * (i + 1)}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
