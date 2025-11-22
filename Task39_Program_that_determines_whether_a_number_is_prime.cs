5141c0ad707d269962025b748db19ca464519181
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-22 09:24:39 +0300

    Task39: Program that determines whether a number is prime

---
+++ Task39_Program_that_determines_whether_a_number_is_prime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 5: Напишите программу, которая определяет, является ли число простым.
+    internal class Program
+    {
+        public static string number;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            number = Console.ReadLine();
+            int num = int.Parse(number);
+            while (num <= 1)
+            {
+                Console.WriteLine("Вы ввели неверное число");
+                Console.WriteLine("Введите число:");
+                number = Console.ReadLine();
+                num = int.Parse(number);
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Число не является простым");
+                    return;
+                }
+            }
+            Console.WriteLine("Вы ввели простое число");
+            Console.ReadKey();
+        }
+    }
+}
