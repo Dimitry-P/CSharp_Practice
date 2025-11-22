d164aea471567c95be5c2546fa4af263d1d05e26
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-22 09:30:47 +0300

    Task41: Program that calculates the square of a number entered by the user

---
+++ Task41_Program_that_calculates_the_square_of_a_number_entered_by_the_user.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 7: Напишите программу, которая вычисляет квадрат числа, введенного пользователем.
+    internal class Program
+    {
+        public static int y = 2;
+        public static string number;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            number = Console.ReadLine();
+            int result = int.Parse(number);
+
+            Console.WriteLine("Квадрат числа равен " + Math.Pow(result, y));
+            Console.ReadKey();
+        }
+    }
+}
+
