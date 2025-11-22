1b56873b8a3551ba61f544848847d55dcf9c5ce0
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-22 09:28:02 +0300

    Task40: Program that checks whether the input string is a palindrome

---
+++ Task40_Program_that_checks_whether_the_input_string_is_a_palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 6: Напишите программу, которая проверяет, является ли введенная строка палиндромом.
+    internal class Program
+    {
+        static List<char> list;
+        public static string word;
+
+        static void Main(string[] args)
+        {
+            list = new List<char>();
+            Console.WriteLine("Введите слово:");
+            word = Console.ReadLine();
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                char perem = word[i];
+
+                list.Add(perem);
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] != word[i])
+                {
+                    Console.WriteLine("Вы ввели НЕ палиндром");
+                    return;
+                }
+            }
+            Console.WriteLine("ЭТО палиндром");
+            Console.ReadKey();
+        }
+    }
+}
