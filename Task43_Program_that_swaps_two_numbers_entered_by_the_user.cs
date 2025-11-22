16199a593e903fb4eeda2a07bed7298347525e79
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-22 09:36:44 +0300

    Task43: Program that swaps two numbers entered by the user

---
+++ Task43_Program_that_swaps_two_numbers_entered_by_the_user.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 9: Напишите программу, которая меняет местами
+        //два числа, введенные пользователем.
+        public static string num;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа через запятую:");
+            num = Console.ReadLine();
+
+            string[] pieces = num.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = pieces.Length - 1; i >= 0; i--)
+            {
+                Console.Write(pieces[i]);
+                if (i == pieces.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+        }
+    }
+}
