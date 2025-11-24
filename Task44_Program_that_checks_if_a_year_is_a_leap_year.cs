c56d8ee87828c4a0f35f8b16cc55a2ec1a392d02
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-24 12:14:45 +0300

    Task44: Program that checks if a year is a leap year

---
+++ Task44_Program_that_checks_if_a_year_is_a_leap_year.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 11: Напишите программу, которая проверяет, является ли год високосным.
+        public static string year;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год");
+            year = Console.ReadLine();
+            int num = int.Parse(year);
+            if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+            {
+                Console.WriteLine(num + " Этот год високосный");
+                return;
+            }
+            Console.WriteLine(num + " Этот год НЕ високосный");
+        }
+    }
+}
