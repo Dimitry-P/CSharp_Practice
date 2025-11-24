431e2abd412dac3aeba32e031ac0819a95b27735
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-24 12:49:01 +0300

    Task47: Program that determines whether a number is even or odd

---
+++ Task47_Program_that_determines_whether_a_number_is_even_or_odd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        // •	Задача 15: Напишите программу, которая
+        // определяет, является ли число четным или нечетным.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string num = Console.ReadLine();
+            int number = int.Parse(num);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("число чётное");
+                return;
+            }
+            Console.WriteLine("число НЕчётное");
+        }
+    }
+}
