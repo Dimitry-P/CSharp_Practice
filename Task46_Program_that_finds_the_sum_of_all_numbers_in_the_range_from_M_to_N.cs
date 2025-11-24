1457aa0351b4bde77290a2aa0f2038b88bc40b96
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-24 12:46:08 +0300

    Task46: Program that finds the sum of all numbers in the range from M to N

---
+++ Task46_Program_that_finds_the_sum_of_all_numbers_in_the_range_from_M_to_N.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        //•	Задача 14: Напишите программу, которая
+        //находит сумму всех чисел в диапазоне от M до N.
+        public static int result = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальное и конечное число через запятую");
+            string num = Console.ReadLine();
+            string[] pieces = num.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            int m = int.Parse(pieces[0]);
+            int n = int.Parse(pieces[1]);
+            for (int i = m; i < n + 1; i++)
+            {
+                result += i;
+            }
+            Console.WriteLine("Сумма чисел равна: " + result);
+        }
+    }
+}
