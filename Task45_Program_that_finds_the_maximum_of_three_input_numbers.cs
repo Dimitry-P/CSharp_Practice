02885b4d6142720ac2c1a24a133033f8f84ee025
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-24 12:41:59 +0300

    Task45: Program that finds the maximum of three input numbers

---
+++ Task45_Program_that_finds_the_maximum_of_three_input_numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp_Practice
+{
+    //•	Задача 12: Напишите программу, которая находит
+    //максимальное из трех введенных чисел.
+    internal class Program
+    {
+        public static string num;
+        public static int result = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите через запятую три числа:");
+            num = Console.ReadLine();
+            string[] pieces = num.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int l = 0; l < pieces.Length; l++)
+            {
+                if (result < int.Parse(pieces[l]))
+                {
+                    result = int.Parse(pieces[l]);
+                }
+            }
+            Console.WriteLine("Наибольшее число: " + result);
+            Console.ReadKey();
+        }
+    }
+}
