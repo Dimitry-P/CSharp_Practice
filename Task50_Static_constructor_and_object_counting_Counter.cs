711ff60462b9465c354a7ca87cdcec408fdb5018
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-26 21:08:36 +0300

    Task50: Static constructor and object counting (Program.cs + Counter.cs)

---
+++ Task50_Static_constructor_and_object_counting_Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    class Counter
+    {
+        public static int amountOfObjects;
+        static Counter()
+        {
+            Console.WriteLine("Статический конструктор вызван");
+            amountOfObjects = 0;
+        }
+
+        public Counter()
+        {
+            amountOfObjects++;
+        }
+        public static void ShowInfo()
+        {
+            Console.WriteLine("Создано объектов: " + amountOfObjects);
+        }
+    }
+}
---
+++ Task50_Static_constructor_and_object_counting_Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество: ");
+            int number = int.Parse(Console.ReadLine());
+            Counter[] counters = new Counter[number];
+            for (int i = 0; i < number; i++)
+            {
+                counters[i] = new Counter();
+            }
+            Counter.ShowInfo();
+        }
+    }
+}
+
+//Задача 3: Статический конструктор и подсчет объектов
+// Создайте класс Counter с массивом объектов, который подсчитывает,
+// сколько объектов было создано. Используйте статический
+// конструктор для инициализации.
+//Ввод:
+//3
+//Вывод:
+//Статический конструктор вызван.
+//Создано объектов: 3
