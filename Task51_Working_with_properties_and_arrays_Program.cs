fe3591bfe0a493ec1ebb688938438d64fdcada23
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-26 21:25:07 +0300

    Task51: Working with properties and arrays. A Student class that stores a student's name and grades. An array of five students and outputting information about each student using a loop (Program.cs + Student.cs)

---
+++ Task51_Working_with_properties_and_arrays_Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        // Methods
+        private static void Main(string[] args)
+        {
+            Console.Write("Введите количество студентов: ");
+            int num = int.Parse(Console.ReadLine());// вводим кол-во студентов
+            Student[] studentArray = new Student[num];// создаём массив с типом Student и с той длиной
+                                                      // которую пользователь ввёл
+            for (int i = 0; i < num; i++)// запуск цикла для ввода пользователем имен и оценок студентов
+            {
+                Student.threeGradesAreCorrect = 0;// обнуление поля, которое подсчитывает, что
+                //кол-во введённых пользователем элементов равно 3 и все они соответствуют требованиям
+                Console.WriteLine($"Введите имя {i + 1}-ого студента:");// вывод сообщения с просьбой ввести имя студентов
+                string name = Console.ReadLine();// в этот стринг кладётся введённое пользователем имя студента
+                do// будет выводится сообщение с просьбой ввести три оценки для конкретного студента до тех пор,
+                  // пока не будут введены соответствующие требованиям значения. 
+                {
+                    Console.WriteLine("Введите через пробел три оценки: ");
+                    string grade = Console.ReadLine();//если все три значения введены верно, то помещаем
+                    // строчку с тремя оценками в поле grade
+                    studentArray[i] = new Student(name, grade);//создаём объект для созданного нами массива,
+                    // в шаблон Student (в конструктор) передаём в аргументе введённое имя и строку с оценками
+                }
+                while (Student.threeGradesAreCorrect != 3);// условие при котором будет повторятся вышеописанное действие
+            }
+            Console.WriteLine("Информация о студентах:");
+            foreach (Student student in studentArray) // проходимся циклом по массиву studentArray
+                                                      // и каждый объект кладём в поле student
+            {
+                student.ShowInfo();// для каждого объекта вызываем метод ShowInfo
+            }
+        }
+    }
+}
---
+++ Task51_Working_with_properties_and_arrays_Student.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    public class Student
+    {
+        // Fields
+        public double averageGrade = 0.0;// поле со средним значением трёх оценок каждого студента
+        public static int threeGradesAreCorrect;//задаём поле, обозначающее, что все три значения для одного
+        // студента введены корректно
+        private int[] grades = new int[3];// массив из трёх ячеек, куда помещаются три введённых пользователем оценки
+
+        // Methods
+        public Student(string name, string grade)// конструктор класса, который принимает два параметра
+        {
+            this.Name = name;// в Name помещается переданное имя
+            this.Grade = grade;// в Grade помещается переданная строчка с оценками
+        }
+
+        public void Calculate()// метод для сложения всех трех оценок и деления их на 3,
+                               // а также для обрезки десятичных чисел после запятой. 
+                               // после запятой остаётся только две цифры. 
+        {
+            for (int i = 0; i < this.grades.Length; i++)
+            {
+                this.averageGrade += this.grades[i];// сложение
+            }
+            this.averageGrade /= (double)this.grades.Length;// деление
+            this.averageGrade = Math.Truncate((double)(this.averageGrade * 100.0)) / 100.0;// приведение
+            //десятичного числа
+        }
+
+        public void ShowInfo()// метод для отображения имени и среднего балла каждого студента
+        {
+            Console.WriteLine($"{this.Name}: Средний балл = {this.averageGrade}");
+        }
+
+        // Properties
+        public string Name { get; set; }// свойства для передаваемых параметров.
+        // сначала параметры поступают в конструктор, далее сюда в свойства.
+        // здесь проверяется переданное имя студента
+
+        public string Grade // свойство для обработки переданной строчки с оценками студента
+        {
+            get =>
+                string.Join<int>(" ", this.grades);//в геттере соединяем в строку каждые три оценки,
+            // переданные в массив grades
+            set// в сеттере делаем проверку переданного значения
+            {
+                char[] separator = new char[] { ' ' };// сначала создаём массив, состоящий из пустой строки
+                string[] strArray = value.Split(separator);// затем дробим введённую пользователем строку
+                // на элементы и помещаем их в стринговый массив
+                if (strArray.Length == 3)// проверям чтобы введённых элементов было ровно три
+                {
+                    for (int i = 0; i < 3; i++)// запуск цикла 
+                    {
+                        if (int.TryParse(strArray[i], out _))// проверка чтобы каждое введенное значение 
+                                                             //являлось числом
+                        {
+                            int num3 = int.Parse(strArray[i]);// в локальную переменную num3 помещаем каждый элемент,
+                            // трансформированный из строки в число
+                            if ((num3 < 1) || (num3 > 5))// условие, чтобы введённое число было в пределах от 1 до 5
+                            {
+                                Console.WriteLine("Оценки должны быть от 1 до 5");// в противном случае будет появляться
+                                // данное сообщение
+                                return;// выход из цикла
+                            }
+                            threeGradesAreCorrect++;// если введённая оуенка соответствует требованиям, то поле
+                            // threeGradesAreCorrect увеличиваем на единицу
+                            this.grades[i] = num3;// и помещаем нашу оценку в массив grades
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели НЕ число!");
+                            return;// если было введено нечисловое значение, то выходим из цикла
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вам нужно ввести ТРИ оценки!");// если введённых значений НЕ три,
+                    // то будет появляться данное сообщение
+                }
+                this.Calculate(); // запуск метода для сложения всех трех оценок и деления их на 3
+            }
+        }
+    }
+}
