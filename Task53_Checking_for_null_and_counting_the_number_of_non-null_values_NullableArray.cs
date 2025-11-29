b7a3605ada879bcab96aa3afe27402607eec4a4e
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-29 12:22:21 +0300

    Task53: Checking for null and counting the number of non-null values (Program.cs + NullableArray.cs)

---
+++ Task53_Checking_for_null_and_counting_the_number_of_non-null_values_NullableArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    public class NullableArray
+    {
+        public static int totalAmount = 0;
+        public static int countOfElements = 0;
+        private int ValueHasExistingWord = 0;
+        public string InputValue
+        {
+            set
+            {
+                if (value != "")
+                {
+                    ValueHasExistingWord++;
+                }
+            }
+        }
+        public NullableArray(string inputValue)
+        {
+            InputValue = inputValue;
+        }
+        public void Calculate(int amountOfWords)
+        {
+            countOfElements++;
+            if (ValueHasExistingWord == 1) totalAmount++;
+            if (countOfElements == amountOfWords) Console.WriteLine(totalAmount);
+        }
+    }
+}
---
+++ Task53_Checking_for_null_and_counting_the_number_of_non-null_values_Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Какое кол-во элементов вы хотите ввести: ");
+            int amountOfWords = int.Parse(Console.ReadLine());
+            NullableArray[] existingWord = new NullableArray[amountOfWords];
+            for (int i = 0; i < amountOfWords; i++)
+            {
+                Console.Write($"Введите элемент {i + 1} или нажмите Enter: ");
+                string inputValue = Console.ReadLine();
+                existingWord[i] = new NullableArray(inputValue);
+            }
+            Console.Write($"Элементов в массиве не равных null: ");
+            foreach (NullableArray result in existingWord)
+            {
+                result.Calculate(amountOfWords);
+            }
+        }
+    }
+}
+
+//Задача 2: Проверка null и подсчет количества ненулевых значений
+//Создайте класс NullableArray, который хранит массив строк.
+//Реализуйте метод, который подсчитывает,
+//сколько элементов в массиве не равны null.
+//Ввод:
+//Элемент 1: Hello
+//Элемент 2: World
+//Элемент 3: 
+//Вывод:
+//Количество ненулевых значений: 2
