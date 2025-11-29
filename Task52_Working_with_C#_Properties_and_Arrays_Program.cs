2d690e24d8fb153695e2b898324949333f4d6040
Dimitry-P <142058310+Dimitry-P@users.noreply.github.com>
2025-11-29 12:16:11 +0300

    Task52: Working with C# Properties and Arrays (Program.cs + Student.cs)

---
+++ Task52_Working_with_C#_Properties_and_Arrays_Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    internal class Program
+    {
+        public static Student student;
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите имя студента: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите через пробел три оценки: ");
+                string marks = Console.ReadLine();
+                student = new Student(name, marks);
+            }
+            Student.ShowNameAndAge();
+        }
+    }
+}
+//Задача 1: Работа со свойствами и массивами
+//Создайте класс Student, который хранит имя и оценки ученика.
+//Создайте массив из пяти учеников и выведите информацию
+//о каждом из них, используя цикл.
+//Ввод:
+//Имя студента: Alice
+//Оценки: 5 4 3
+//Имя студента: Bob
+// Оценки: 4 4 5
+// Вывод:
+//Alice: Средний балл = 4.00
+//Bob: Средний балл = 4.33
---
+++ Task52_Working_with_C#_Properties_and_Arrays_Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Practice
+{
+    public class Student
+    {
+        static List<string> namesList = new List<string>();
+        static List<string> ageList = new List<string>();
+
+        public string Name
+        {
+            get; set;
+        }
+
+        private string age = "Undefined";
+        public string Age
+        {
+            set
+            {
+                int middle;
+                string[] pieces = value.Split(new char[] { ' ' });
+                for (int k = 0; k < 3; k++)
+                {
+                    middle = int.Parse(pieces[k]);
+                    if (middle < 1 || middle > 5)
+                    {
+                        Console.WriteLine("Оценки должны быть от 1 до 5");
+                        break;
+                    }
+                    else age = value;
+                }
+            }
+            get { return age; }
+        }
+        public Student(string name, string age)
+        {
+            Name = name;
+            Age = age;
+            namesList.Add(Name);
+            ageList.Add(Age);
+        }
+        public static void ShowNameAndAge()
+        {
+            int average = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    string[] pieces = ageList[i].Split(new char[] { ' ' });
+                    average += int.Parse(pieces[k]);
+                }
+                Console.WriteLine($"{namesList[i]}: Средний бал = {average / 3}");
+                average = 0;
+            }
+        }
+    }
+}
